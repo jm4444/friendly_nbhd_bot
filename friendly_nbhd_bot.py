6790d251e47d11db4b8855948af70b5223e9972b
Justin Meredith <jm.4444@icloud.com>
2018-05-24 15:51:13 -0500

    A new function called 'store_all_upload_ids' has been added to the YouTubeChannel class. This function goes through every video the channel has uploaded, finds that video's ID, and stores that ID. This is so that the bot keeps track of all previously uploaded videos, which prevents it from mistakenly reposting an old video to the subreddit. There is a known error with line 72 in the program that is being resolved.

--- friendly_nbhd_bot.py
+++ friendly_nbhd_bot.py
@@ -34,6 +34,15 @@ nbhd_subreddit = reddit.subreddit('ScratchpadsScratchpad')    # Currently set to
 
 
 
+#  -  -  -  -  -  -  -  -  -  -  -  -  - FUNCTIONS -  -  -  -  -  -  -  -  -  -  -  -  -  #
+
+# @ensureConnection
+def getPlaylistItems(youtube, uploads_playlist):
+    https = youtube.playlistItems().list(part = "snippet, contentDetails", maxResults = 50, playlistId = uploads_playlist)
+    return https, https.execute()
+
+
+
 #  -  -  -  -  -  -  -  -  -  -  -  -  - CLASSES -  -  -  -  -  -  -  -  -  -  -  -  -  #
 
 class YouTubeChannel:
@@ -42,8 +51,28 @@ class YouTubeChannel:
         self.uploads_playlist = uploads_playlist
         self.number_of_uploads = youtube.playlistItems().list(part="snippet, ContentDetails", maxResults=3, playlistId=uploads_playlist).execute()['pageInfo']['totalResults']
         print(self.channel_name + " has uploaded " + str(self.number_of_uploads) + " videos.")
+        self.store_all_upload_ids()
+        print("There are " + str(len(self.upload_ids)) + " uploads IDs.")
 
+    def store_all_upload_ids(self):
+        self.upload_ids = []
+        https, uploads = getPlaylistItems(youtube, self.uploads_playlist)
+        more_videos = True
+        # print(uploads)
 
+        while more_videos:
+            for item in uploads['items']:    # Adds the video ID of every video to the upload_ids variable
+                upload_id = item['contentDetails']['videoId']
+                self.upload_ids.append(upload_id)
+
+            if len(self.upload_ids) >= self.number_of_uploads:    # Ends the loop once every ID is known and stored
+                more_videos = False
+
+            if 'nextPageToken' in uploads.keys():    # Goes to the next page of uploads, if there is one
+                https, uploads = getNextPlPage(youtube, https, uploads)
+            elif more_videos:
+                verbose('Warning: Loop reached while generating video list!', override=False)
+                https, uploads = getPlaylistItems(youtube, self.upload_IDs)
 
 
 
